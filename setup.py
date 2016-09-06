ce871ee127d8f4708f03ca6db46d5acf53aa386e
Peter Wagner <pwagner@pebble.com>
2016-09-06 13:33:13 -0400

    Change author_email

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='spacel-provision',
-      version='0.0.1',
+      version='0.1.0',
       description='Space Elevator provisioner',
       long_description=open('README.md').read(),
       url='https://github.com/pebble/spacel-provision',
       author='Pebble WebOps',
-      author_email='webops_team@pebble.com',
+      author_email='webops@pebble.com',
       license='MIT',
       package_dir={'': 'src'},
       packages=find_packages('src', exclude=['test']),
